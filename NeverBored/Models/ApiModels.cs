1afdf5d35186da78b448e448492d16f08141d1f1
Mahodi313 <MehdiAli15@outlook.com>
2024-02-09 17:54:13 +0100

    InitialCreate

---
+++ NeverBored/Models/ApiModels.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace NeverBored.Models
+{
+    public class Root
+    {
+        [JsonProperty("activity")]
+        public string Activity { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("link")]
+        public string Link { get; set; }
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+    }
+}
