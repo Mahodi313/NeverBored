c79b1f3263af68e35bd4f27386563669f082b47d
Mahodi313 <MehdiAli15@outlook.com>
2024-02-11 18:24:37 +0100

    FavoriteActivityLogicAdd

--- NeverBored/Pages/ActivityView/Index.cshtml.cs
+++ NeverBored/Pages/ActivityView/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using NeverBored.Database;
 using NeverBored.Models;
 using NeverBored.Services;
+using NeverBored.Utilities;
 using System.Data.SqlTypes;
 
 namespace NeverBored.Pages.ActivityView
@@ -10,12 +11,15 @@ namespace NeverBored.Pages.ActivityView
     public class IndexModel : PageModel
     {
         private readonly AppDbContext _dbContext;
+        private readonly CookieService _cookieService;
 
         public List<ActivityModel> Activites { get; set; } = new();
 
-        public IndexModel(AppDbContext dbContext)
+        public IndexModel(AppDbContext dbContext, CookieService cookieService)
         {
+            _cookieService = cookieService;
             _dbContext = dbContext;
+
         }
 
         public async Task OnGet()
@@ -23,6 +27,12 @@ namespace NeverBored.Pages.ActivityView
             ActivityRepository activityRepo = new(_dbContext);
 
             Activites = await activityRepo.GetAll();
+        }
+
+        public async Task<IActionResult> OnPost(int id)
+        {
+            _cookieService.AddFavoriteActivity(id);
+            return RedirectToPage();
         }
     }
 }
